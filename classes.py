3cf7c18285a6ab24af30e3832b6e253597855197
ajh387 <ajh387@drexel.edu>
2020-08-20 23:34:43 -0400

    Added movement logic for the queen, rook, bishop, and knight Added standard and en passant movement logic for Pawn Pawn still needs promotion logic

--- classes.py
+++ classes.py
@@ -1,91 +1,542 @@
 from abc import ABC, abstractmethod
 import numpy as np
 
+
 class Board:
     def __init__(self):
         self.board = np.empty((8,8), Piece)
         self.shape = self.board.shape
+        self.en_passant = None
+        self.en_passant_counter = 0
 
     def __str__(self):
         return str(self.board)
 
+    # Returns the contents of the specified space
     def get_space(self, x, y):
         return self.board[y][x]
 
-    def add(self, piece):
+    # Adds a piece to the board
+    def add_piece(self, piece):
         self.board[piece.y][piece.x] = piece
 
-    def fill_board(self):
-        self.add(Rook("White", 0, 0))
-        self.add(Knight("White", 1, 0))
-        self.add(Bishop("White", 2, 0))
-        self.add(King("White", 3, 0))
-        self.add(Queen("White", 4, 0))
-        self.add(Bishop("White", 5, 0))
-        self.add(Knight("White", 6, 0))
-        self.add(Rook("White", 7, 0))
-        for i in range(8):
-            self.add(Pawn("White", i, 1))
-        for i in range(8):
-            self.add(Pawn("Black", i, 6))
-        self.add(Rook("Black", 0, 7))
-        self.add(Knight("Black", 1, 7))
-        self.add(Bishop("Black", 2, 7))
-        self.add(King("Black", 3, 7))
-        self.add(Queen("Black", 4, 7))
-        self.add(Bishop("Black", 5, 7))
-        self.add(Knight("Black", 6, 7))
-        self.add(Rook("Black", 7, 7))
+    # Removes a piece from the board
+    def remove_piece(self, piece):
+        self.board[piece.y][piece.x] = None
 
+    # Places all pieces in their starting positions
+    def fill_board(self):
+        self.add_piece(Rook("Black", 0, 0))
+        self.add_piece(Knight("Black", 1, 0))
+        self.add_piece(Bishop("Black", 2, 0))
+        self.add_piece(King("Black", 3, 0))
+        self.add_piece(Queen("Black", 4, 0))
+        self.add_piece(Bishop("Black", 5, 0))
+        self.add_piece(Knight("Black", 6, 0))
+        self.add_piece(Rook("Black", 7, 0))
+        for i in range(8):
+            self.add_piece(Pawn("Black", i, 1))
+        for i in range(8):
+            self.add_piece(Pawn("White", i, 6))
+        self.add_piece(Rook("White", 0, 7))
+        self.add_piece(Knight("White", 1, 7))
+        self.add_piece(Bishop("White", 2, 7))
+        self.add_piece(King("White", 3, 7))
+        self.add_piece(Queen("White", 4, 7))
+        self.add_piece(Bishop("White", 5, 7))
+        self.add_piece(Knight("White", 6, 7))
+        self.add_piece(Rook("White", 7, 7))
+
+    # Used for testing
     def print_board(self):
         for i in range(8):
             output = ""
             for j in range(8):
-                output += "%13s " % self.board[i][j]
+                output += "| (%d, %d): %12s " % (j, i, self.board[i][j])
             print(output)
 
+
 class Piece(ABC):
+    @abstractmethod
     def __init__(self, color, x, y):
         self.x = x
         self.y = y
         self.color = color
         super().__init__()
 
+    # Returns a list of valid moves for the piece
+    @abstractmethod
+    def get_valid_moves(self, board):
+        # Implement check logic here probably
+        pass
+
+    # Moves the piece to the specified space
+    def move(self, board, new_x, new_y):
+        if board.en_passant_counter > 0:
+            board.en_passant_counter -= 1
+        if board.en_passant_counter == 0 and board.en_passant is not None:
+            board.en_passant.en_passant = False
+            board.en_passant = None
+        board.remove_piece(self)
+        self.x = new_x
+        self.y = new_y
+        board.add_piece(self)
+
     def __str__(self):
         return "%s %s" % (self.color, self.name)
+
 
 class Queen(Piece):
     def __init__(self, color, x, y):
         super().__init__(color, x, y)
         self.name = "Queen"
 
+    def get_valid_moves(self, board):
+        output = []
+
+        # Up
+        new_x = self.x
+        new_y = self.y - 1
+        while True:
+            if new_y < 0:
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_y -= 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Up-Right
+        new_x = self.x + 1
+        new_y = self.y - 1
+        while True:
+            if new_x > (board.shape[1] - 1) or new_y < 0:
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x += 1
+                    new_y -= 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Right
+        new_x = self.x + 1
+        new_y = self.y
+        while True:
+            if new_x > (board.shape[1] - 1):
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x += 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Down-Right
+        new_x = self.x + 1
+        new_y = self.y + 1
+        while True:
+            if new_x > (board.shape[1] - 1) or new_y > (board.shape[0] - 1):
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x += 1
+                    new_y += 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Down
+        new_x = self.x
+        new_y = self.y + 1
+        while True:
+            if new_y > (board.shape[0] - 1):
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_y += 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Down-Left
+        new_x = self.x - 1
+        new_y = self.y + 1
+        while True:
+            if new_x < 0 or new_y > (board.shape[0] - 1):
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x -= 1
+                    new_y += 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Left
+        new_x = self.x - 1
+        new_y = self.y
+        while True:
+            if new_x < 0:
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x -= 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Up-Left
+        new_x = self.x - 1
+        new_y = self.y - 1
+        while True:
+            if new_x < 0 or new_y < 0:
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x -= 1
+                    new_y -= 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        return output
+
+
 class Pawn(Piece):
     def __init__(self, color, x, y):
         super().__init__(color, x, y)
         self.name = "Pawn"
+        self.moved = False
+        self.en_passant = False
+        if color == "White":
+            self.direction = -1
+        else:
+            self.direction = 1
+
+    def get_valid_moves(self, board):
+        output = []
+
+        new_x = self.x
+        new_y = self.y + self.direction
+        if board.get_space(new_x, new_y) is None:
+            output.append((new_x, new_y))
+            if not self.moved and board.get_space(new_x, new_y + self.direction) is None:
+                output.append((new_x, new_y + self.direction))
+
+        new_x = self.x - 1
+        new_y = self.y + self.direction
+        if new_x >= 0:
+            space = board.get_space(new_x, new_y)
+            if space is not None and space.color != self.color:
+                output.append((new_x, new_y))
+
+        new_x = self.x + 1
+        new_y = self.y + self.direction
+        if new_x < board.shape[1]:
+            space = board.get_space(new_x, new_y)
+            if space is not None and space.color != self.color:
+                output.append((new_x, new_y))
+
+        if board.en_passant is not None and board.en_passant.color != self.color and board.en_passant.y == self.y:
+            if board.en_passant.x == self.x - 1:
+                output.append((self.x - 1, self.y + self.direction))
+            elif board.en_passant.x == self.x + 1:
+                output.append((self.x + 1, self.y + self.direction))
+
+        return output
+
+    def move(self, board, new_x, new_y):
+        if new_x != self.x and board.get_space(new_x, new_y) is None:
+            board.remove_piece(board.get_space(new_x, new_y - self.direction))
+        super().move(board, new_x, new_y)
+        if not self.moved and (new_y == 3 or new_y == 4):
+            self.en_passant = True
+            board.en_passant = self
+        self.moved = True
+        if new_y == 0 or new_y == 7:
+            # Implement promotion here
+            pass
+
 
 class Rook(Piece):
     def __init__(self, color, x, y):
         super().__init__(color, x, y)
         self.name = "Rook"
+        self.moved = False
+
+    def get_valid_moves(self, board):
+        output = []
+
+        # Up
+        new_x = self.x
+        new_y = self.y - 1
+        while True:
+            if new_y < 0:
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_y -= 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Right
+        new_x = self.x + 1
+        new_y = self.y
+        while True:
+            if new_x > (board.shape[1] - 1):
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x += 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Down
+        new_x = self.x
+        new_y = self.y + 1
+        while True:
+            if new_y > (board.shape[0] - 1):
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_y += 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Left
+        new_x = self.x - 1
+        new_y = self.y
+        while True:
+            if new_x < 0:
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x -= 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        return output
+
+    def move(self, board, new_x, new_y):
+        super().move(board, new_x, new_y)
+        self.moved = True
+
 
 class Bishop(Piece):
     def __init__(self, color, x, y):
         super().__init__(color, x, y)
         self.name = "Bishop"
 
+    def get_valid_moves(self, board):
+        output = []
+
+        # Up-Right
+        new_x = self.x + 1
+        new_y = self.y - 1
+        while True:
+            if new_x > (board.shape[1] - 1) or new_y < 0:
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x += 1
+                    new_y -= 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Down-Right
+        new_x = self.x + 1
+        new_y = self.y + 1
+        while True:
+            if new_x > (board.shape[1] - 1) or new_y > (board.shape[0] - 1):
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x += 1
+                    new_y += 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Down-Left
+        new_x = self.x - 1
+        new_y = self.y + 1
+        while True:
+            if new_x < 0 or new_y > (board.shape[0] - 1):
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x -= 1
+                    new_y += 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        # Up-Left
+        new_x = self.x - 1
+        new_y = self.y - 1
+        while True:
+            if new_x < 0 or new_y < 0:
+                break
+            else:
+                space = board.get_space(new_x, new_y)
+                if space is None:
+                    output.append((new_x, new_y))
+                    new_x -= 1
+                    new_y -= 1
+                else:
+                    if space.color != self.color:
+                        output.append((new_x, new_y))
+                    break
+
+        return output
+
+
 class Knight(Piece):
     def __init__(self, color, x, y):
         super().__init__(color, x, y)
         self.name = "Knight"
 
+    def get_valid_moves(self, board):
+        output = []
+
+        new_x = self.x + 1
+        new_y = self.y - 2
+        if not (new_x > (board.shape[1] - 1) or new_y < 0):
+            space = board.get_space(new_x, new_y)
+            if space is None or space.color != self.color:
+                output.append((new_x, new_y))
+
+        new_x = self.x + 2
+        new_y = self.y - 1
+        if not (new_x > (board.shape[1] - 1) or new_y < 0):
+            space = board.get_space(new_x, new_y)
+            if space is None or space.color != self.color:
+                output.append((new_x, new_y))
+
+        new_x = self.x + 2
+        new_y = self.y + 1
+        if not (new_x > (board.shape[1] - 1) or new_y > (board.shape[0] - 1)):
+            space = board.get_space(new_x, new_y)
+            if space is None or space.color != self.color:
+                output.append((new_x, new_y))
+
+        new_x = self.x + 1
+        new_y = self.y + 2
+        if not (new_x > (board.shape[1] - 1) or new_y > (board.shape[0] - 1)):
+            space = board.get_space(new_x, new_y)
+            if space is None or space.color != self.color:
+                output.append((new_x, new_y))
+
+        new_x = self.x - 1
+        new_y = self.y + 2
+        if not (new_x < 0 or new_y > (board.shape[0] - 1)):
+            space = board.get_space(new_x, new_y)
+            if space is None or space.color != self.color:
+                output.append((new_x, new_y))
+
+        new_x = self.x - 2
+        new_y = self.y + 1
+        if not (new_x < 0 or new_y > (board.shape[0] - 1)):
+            space = board.get_space(new_x, new_y)
+            if space is None or space.color != self.color:
+                output.append((new_x, new_y))
+
+        new_x = self.x - 2
+        new_y = self.y - 1
+        if not (new_x < 0 or new_y < 0):
+            space = board.get_space(new_x, new_y)
+            if space is None or space.color != self.color:
+                output.append((new_x, new_y))
+
+        new_x = self.x - 1
+        new_y = self.y - 2
+        if not (new_x < 0 or new_y < 0):
+            space = board.get_space(new_x, new_y)
+            if space is None or space.color != self.color:
+                output.append((new_x, new_y))
+
+        return output
+
+
 class King(Piece):
     def __init__(self, color, x, y):
         super().__init__(color, x, y)
         self.name = "King"
+        self.moved = False
+
+    def get_valid_moves(self, board):
+        # Implement
+        pass
+
+    def move(self, board, new_x, new_y):
+        # Implement
+        super().move(board, new_x, new_y)
+        self.moved = True
 
 
+# Testing
 if __name__ == "__main__":
     board = Board()
     board.fill_board()
+    test_piece = Pawn("White", 2, 6)
+    board.add_piece(test_piece)
+    test_piece.move(board, 2, 5)
+    test_piece.move(board, 2, 4)
+    test_piece.move(board, 2, 3)
+    board.get_space(3, 1).move(board, 3, 3)
+    test_piece.move(board, 3, 2)
     board.print_board()
+    print(test_piece.get_valid_moves(board))
